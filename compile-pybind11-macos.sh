52de7fad9e274867b3dca54df68a8e5d61bb0993
yrom1 <ryanm.inbox@gmail.com>
2022-04-15 08:30:43 -0400

    Add logic to strip extension off path for compile script, tested

--- compile-pybind11-macos.sh
+++ compile-pybind11-macos.sh
@@ -6,4 +6,4 @@ Usage: bash compile-pybind11-macos [file.cpp]
 _EOT_
 exit 1
 fi
-c++ -O3 -Wall -shared -std=c++11 -undefined dynamic_lookup $(python3-config --includes) -Iextern/pybind11/include $1 -o example$(python3-config --extension-suffix)
+c++ -O3 -Wall -shared -std=c++11 -undefined dynamic_lookup $(python3-config --includes) -Iextern/pybind11/include $1 -o $(basename $1 .cpp)$(python3-config --extension-suffix)
